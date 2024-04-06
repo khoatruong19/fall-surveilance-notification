0f0dbd479622d803958eeab27327281a4f34b7d5
khoatruong19 <khoa-truong@cybozu.vn>
2024-04-06 19:51:19 +0700

    feat: fix bug register token

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import fastify, { FastifyReply, FastifyRequest } from "fastify";
 import { admin } from "./utils/firebase";
 import * as dotenv from "dotenv";
-import { config } from './utils/config';
+import { config } from "./utils/config";
 import { getAllUserDevices, getUserDevice } from "./db/queries";
-import { AddUserDeviceBody, AppPushNotificationBody, addUserDeviceSchema, appPushNotificationSchema } from "./utils/inputSchema";
+import {
+  AddUserDeviceBody,
+  AppPushNotificationBody,
+  addUserDeviceSchema,
+  appPushNotificationSchema,
+} from "./utils/inputSchema";
 import { insertUserDevice } from "./db/mutations";
 import { MessagingPayload } from "firebase-admin/lib/messaging/messaging-api";
 import { NotificationCode, buildMessagePayload } from "./utils/message";
@@ -16,70 +21,101 @@ app.get("/", async () => {
   return "Server is running OK v3!";
 });
 
-app.post('/register-token', {schema: addUserDeviceSchema}, async (request: FastifyRequest<{
-  Body: AddUserDeviceBody
-}>, reply: FastifyReply) => {
-  const { userId, token} = request.body
-  console.log({userId, token})
-  const existingUserDevice = await getUserDevice(userId, token)
-  if(existingUserDevice) return reply.status(400).send({
-    code: 400,
-    message: "User with this device is existed!"
-  })
+app.post(
+  "/register-token",
+  { schema: addUserDeviceSchema },
+  async (
+    request: FastifyRequest<{
+      Body: AddUserDeviceBody;
+    }>,
+    reply: FastifyReply
+  ) => {
+    const { userId, token } = request.body;
 
-  const newUserDevice =  await insertUserDevice(request.body)
-  if(existingUserDevice) return reply.status(500).send({
-    code: 500,
-    message: "Server errors!"
-  })
+    const existingUserDevice = await getUserDevice(userId, token);
+    if (existingUserDevice)
+      return reply.status(400).send({
+        code: 400,
+        message: "User with this device is existed!",
+      });
 
-  return reply.status(201).send({
-    code: 201,
-    message: "Token registered successfully!",
-    data: newUserDevice
-  })
-})
+    const newUserDevice = await insertUserDevice(request.body);
+    if (existingUserDevice)
+      return reply.status(500).send({
+        code: 500,
+        message: "Server errors!",
+      });
 
-app.get('/user-devices/:userId', async (request: FastifyRequest<{
-  Params: {
-    userId: string
+    return reply.status(201).send({
+      code: 201,
+      message: "Token registered successfully!",
+      data: newUserDevice,
+    });
   }
-}>, reply: FastifyReply) => {
-  const { userId } = request.params
- 
-  return reply.send({
-    code: 200,
-    message: "User devices",
-    data: await getAllUserDevices(userId)
-  })
-})
+);
 
-app.post("/app-notify", {schema: appPushNotificationSchema},async (request: FastifyRequest<{
- Body: AppPushNotificationBody
-}>, reply: FastifyReply) => {
-  const messaging = admin.messaging();
+app.get(
+  "/user-devices/:userId",
+  async (
+    request: FastifyRequest<{
+      Params: {
+        userId: string;
+      };
+    }>,
+    reply: FastifyReply
+  ) => {
+    const { userId } = request.params;
 
-  const { receiverIDs, eventCode, meta } = request.body
-
-  const payload: MessagingPayload = buildMessagePayload(eventCode as NotificationCode, meta)
-  try {
-    await Promise.all(receiverIDs.map(async id => {
-      const userDevices = await getAllUserDevices(id);
-
-      return Promise.all(userDevices.map(async item => messaging.sendToDevice(item.deviceToken!, payload)));
-  }));
-  } catch (error) {
-    return reply.status(500).send({
-      code: 500,
-      message: "Server errors!",
-    })
+    return reply.send({
+      code: 200,
+      message: "User devices",
+      data: await getAllUserDevices(userId),
+    });
   }
+);
 
-  return reply.send({
-    code: 200,
-    message: "Push notification successfully!",
-  })
-});
+app.post(
+  "/app-notify",
+  { schema: appPushNotificationSchema },
+  async (
+    request: FastifyRequest<{
+      Body: AppPushNotificationBody;
+    }>,
+    reply: FastifyReply
+  ) => {
+    const messaging = admin.messaging();
+
+    const { receiverIDs, eventCode, meta } = request.body;
+
+    const payload: MessagingPayload = buildMessagePayload(
+      eventCode as NotificationCode,
+      meta
+    );
+    try {
+      await Promise.all(
+        receiverIDs.map(async (id) => {
+          const userDevices = await getAllUserDevices(id);
+
+          return Promise.all(
+            userDevices.map(async (item) =>
+              messaging.sendToDevice(item.deviceToken!, payload)
+            )
+          );
+        })
+      );
+    } catch (error) {
+      return reply.status(500).send({
+        code: 500,
+        message: "Server errors!",
+      });
+    }
+
+    return reply.send({
+      code: 200,
+      message: "Push notification successfully!",
+    });
+  }
+);
 
 const start = async () => {
   try {
--- src/db/queries.ts
+++ src/db/queries.ts
@@ -1,17 +1,17 @@
 import db from ".";
 
 export async function getAllUserDevices(userId: string) {
-    const allUserDevicec = await db.query.user_devices.findMany({
-        where: (item, {eq}) => (eq(item.userId , userId))
-    })
-    return allUserDevicec;
+  const allUserDevicec = await db.query.user_devices.findMany({
+    where: (item, { eq }) => eq(item.userId, userId),
+  });
+  return allUserDevicec;
 }
 
 export async function getUserDevice(userId: string, deviceToken: string) {
-    const userDevice = await db.query.user_devices.findFirst({
-        where: (item, {eq}) => (eq(item.userId , userId) && eq(item.deviceToken , deviceToken))
-    })
+  const userDevice = await db.query.user_devices.findFirst({
+    where: (item, { eq, and }) =>
+      and(eq(item.userId, userId), eq(item.deviceToken, deviceToken)),
+  });
 
-    return userDevice
+  return userDevice;
 }
-
